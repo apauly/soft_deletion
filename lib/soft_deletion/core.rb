f8f4783d6ffb2f245c1206f3d011f2c052e37bc9
Alexander Pauly <alex.pauly@gmx.de>
2019-06-26 20:20:39 +0200

    Debug failing spec

--- lib/soft_deletion/core.rb
+++ lib/soft_deletion/core.rb
@@ -88,7 +88,10 @@ module SoftDeletion
 
     def update_soft_delete_counter_caches(value)
       each_counter_cached_associations do |association|
+        puts "association: #{association}"
+        puts "association.loaded?: #{association.loaded?}"
         association.load_target unless association.loaded?
+        puts "association.target: #{association.target}"
         if association.target
           target = association.target
           target.class.update_counters(target.id, association.reflection.counter_cache_column => value)
